59105a76cc16407d7398621eb87d91f2d4990697
hungnguyen0545 <17520545@gm.uit.edu.vn>
2020-04-14 13:32:15 +0700

    write function to get data from database

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Student extends Model
 {
     protected $table = 'sinhviens';
+
+    public function scopeShowChart()
+    {
+        return DB::table('sinhviens')
+        ->rightJoin('khoas', 'sinhviens.khoa_id', '=', 'khoas.id')
+        ->selectRaw('khoas.id,coalesce(count(sinhviens.id),0) as slsv')
+        ->groupBy('khoas.id')
+        ->get();
+    }
+    
+    public function scopeShowStudent()
+    {
+        return DB::table('sinhviens')
+        ->join('khoas', 'sinhviens.khoa_id', '=', 'khoas.id')
+        ->select('sinhviens.*', 'khoas.tenkhoa');
+    }
 }
