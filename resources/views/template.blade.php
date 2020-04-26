8bd45dc95750e2da8c1607ea2c9251fcb7d7fe21
hungnguyen0545 <17520545@gm.uit.edu.vn>
2020-04-26 22:05:12 +0700

    add new icon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/images/site.webmanifest') }}">
     <link href="{{ asset('/css/template.css') }}" rel="stylesheet" type="text/css">
     @stack('styles')
 </head>
 
 <body>
     <nav class="header navbar navbar-expand-lg navbar-light bg-success">
-        <a class="navbar-brand text-light header-title" href="{{ route('student.index')}}">Student Management </a>
+        <a class="navbar-brand text-light header-title" href="{{ route('students.index')}}">Student Management </a>
         <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
             <li class="nav-item dropdown">
@@ -39,18 +43,6 @@
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" ></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" ></script>
-<script>
-    $(document).ready(function() {
-        $('.alert').fadeOut(2000);
-
-        $('.btn-delete').on('click', function() {
-            if (confirm('Bạn có chắc chắn muốn xoá thông tin này ?')) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-    })
-</script>
+<script src=" {{ asset('js/alert.js')}} "></script>
 @yield('script')
-</html>
+</html> 
