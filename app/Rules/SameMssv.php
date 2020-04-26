ead84977758e5dcfeb5ba1e773f55d7477564614
hungnguyen0545 <17520545@gm.uit.edu.vn>
2020-04-26 22:04:08 +0700

    create new rule to alert when fill same mssv

---
+++ app/Rules/SameMssv.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use App\Sinhviens;
+
+class SameMssv implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $checkMssv = Sinhviens::CheckMssv($value);
+        if($checkMssv != 0)
+        return false;
+        else 
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return "Your's Mssv has been exists ! Please fill other Mssv .";
+    }
+}
