c73cb94f4f03bfa55836fbb99caf58c24b5ce60e
hungnguyen0545 <17520545@gm.uit.edu.vn>
2020-04-26 22:00:40 +0700

    rewrite some funciton and write new checkstar function

--- app/sinhviens.php
+++ app/sinhviens.php
@@ -4,34 +4,58 @@ namespace App;
 
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
-class sinhviens extends Model
+
+class Sinhviens extends Model
 {
     protected $table = 'sinhviens';
     protected $primaryKey='id';
+    const const_value = "Nothi" ;
 
     public function khoas()
     {
-        return $this->belongsto('App\khoas','khoa_id');
+        return $this->belongsto('App\Khoas','khoa_id');
     }
 
-    public function scopeShowChart()
-    {
-        return $this
-        ->rightJoin('khoas', 'sinhviens.khoa_id', '=', 'khoas.id')
-        ->selectRaw('khoas.id,coalesce(count(sinhviens.id),0) as slsv')
-        ->groupBy('khoas.id')
-        ->get();
-    }
-    
     public function scopeShowStudent()
     {
-        return $this::with('khoas');
+        return $this->with('khoas');
     }
-
+    public function scopeShowChart()
+    {
+        return $this->rightJoin('khoas', 'sinhviens.khoa_id', '=', 'khoas.id')
+                ->selectRaw('khoas.id,coalesce(count(sinhviens.id),0) as slsv')
+                ->groupBy('khoas.id')
+                ->get();
+    }
+    
     public function scopeCountStudent()
     {
         return $this->count();
     }
+    public function scopeFetch($query,$agr)
+    {
+        return  $this->ShowStudent()
+                ->WhereRaw("sinhviens.hoten like '%" . $agr . "%'")
+                ->orWhereRaw("sinhviens.mssv like '%" . $agr . "%'")
+                ->orWhereRaw("sinhviens.nghenghiep like '%" . $agr . "%'")
+                ->paginate(5);
+    }
+    public function scopeCountFetch($query,$agr)
+    {
+        return $this->Fetch($agr)->count();
+    }
+    public function scopeCheckMssv($query,$mssv) {
+        return Sinhviens::WhereRaw("mssv like '%".$mssv."%'")->count(); 
+    }
+    public function scopeCheckStar($query , $id)
+    {
+        return Sinhviens::select("id")->find($id);
+    }
+    public function scopeTest()
+    {
+        return Sinhviens::get();
+    }
+   
 }   
+
